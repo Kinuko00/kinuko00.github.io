731f74397377363e60d397891b9b0653cf50488a
Carolina Yang <carolina.yang@avature.net>
2019-07-20 14:20:05 -0300

    Changes

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -317,23 +317,29 @@ function debounce(func, wait, immediate) {
   };
 };
 
+//smooth scroll
 
-//botón Ir arriba
-JavaScript
 $(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-  $('.ir-arriba').click(function(){
-    $('body, html').animate({
-      scrollTop: '0px'
-    }, 300);
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
-
-  $(window).scroll(function(){
-    if( $(this).scrollTop() > 0 ){
-      $('.ir-arriba').slideDown(300);
-    } else {
-      $('.ir-arriba').slideUp(300);
-    }
-  });
-
 });
